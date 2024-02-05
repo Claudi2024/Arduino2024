2ba071777f88036c0eac6046b688ef541f426a4c
Claudi2024 <claudi.gil22@lacetania.cat>
2024-02-05 08:58:20 +0100

    Create Ex5.ino

---
+++ 004 Estructures de Control de Selecció if & switch/Ex5/Ex5.ino
@@ -0,0 +1,40 @@
+/**********************************************************************************
+**                                                                               **
+**                          Control Structures                                   **
+**                                                                               **
+**  05/02/2024                                               Claudi Gil          **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+float co2 = 3.2;
+
+//********** Setup ****************************************************************
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if(co2 < 3.5)
+    { Serial.println("A");}
+  else if (co2 < 6.5)
+    { Serial.println("B");}
+  else if(co2 < 11.1)
+    { Serial.println("C");}
+  else if(co2 < 17.1)
+    { Serial.println("D");}
+  else if(co2 < 38.2)
+    { Serial.println("E");}
+  else if(co2 < 43.2)
+    { Serial.println("F");}
+  else
+    { Serial.println("G");}
+}
+
+
+//********** Loop *****************************************************************
+void loop()   // we need this to be here even though its empty
+{
+}
+
+//********** Funcions *************************************************************
