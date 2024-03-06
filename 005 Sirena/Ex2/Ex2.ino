f7367cc845600bf96a8453f685ed0188629e5c76
Claudi2024 <claudi.gil22@lacetania.cat>
2024-03-06 13:49:26 +0100

    Create Ex2.ino

---
+++ 005 Sirena/Ex2/Ex2.ino
@@ -0,0 +1,36 @@
+
+/**********************************************************************************
+**                                                                               **
+**                                  Sirena                                       **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const byte xiulet = 9;        // donar nom al pin 9 de l’Arduino
+const byte pot0 = A0;
+const byte pot1 = A1;        // donar nom al pin A0 de l’Arduino
+int valPot0;                 // guardar valor del potenciometre  
+int valPot1;
+//********** Setup ****************************************************************
+void setup()
+{
+  pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  valPot0 = analogRead(pot0);    // llegir valor potenciòmetre
+  valPot1 = analogRead(pot1); 
+
+  tone(xiulet, 500,500);  // xiulet de durada valPot0
+  tone(xiulet, 500,500);
+  tone(xiulet, 500,500);
+  tone(xiulet, 500,500);
+  delay(1000);            // esperar valPot0 del xiulet + valPot0 silenci
+}
+
+//********** Funcions *************************************************************
